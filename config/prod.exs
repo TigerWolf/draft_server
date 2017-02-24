017d0576a6b4c5210004b8667b948dd0dd092f8b
Kieran Andrews <kieran.andrews@adelaide.edu.au>
2017-02-24 12:54:15 +1030

    change port config

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :draft_server, DraftServer.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "challengecup.club", port: 80],
+  url: [host: "challengecup.club", port: 8080],
   cache_static_manifest: "priv/static/manifest.json",
   server: true
 
